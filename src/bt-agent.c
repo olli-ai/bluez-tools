b002f1c689c8809c9ab115252652b985c3b4a501
danghuutoan <danghuutoan2002@gmail.com>
2016-11-30 23:45:16 +0700

    update agent

--- src/bt-agent.c
+++ src/bt-agent.c
@@ -113,7 +113,7 @@ static void _adapter_property_changed(GDBusConnection *connection, const gchar *
 
 
     GVariant *properties = NULL;
-    if (g_variant_lookup(changed_properties, DEVICE_DBUS_INTERFACE, "@a{sv}", &properties))
+    if (g_variant_lookup(changed_properties, ADAPTER_DBUS_INTERFACE, "@a{sv}", &properties))
     {
 		g_print("  Connected: %d\n", g_variant_lookup_value(properties, "Connected", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(properties, "Connected", NULL)) : FALSE);
     	// g_print("[%s]\n", g_variant_get_string(g_variant_lookup_value(properties, "Address", NULL), NULL));
@@ -133,15 +133,7 @@ static void _adapter_property_changed(GDBusConnection *connection, const gchar *
     else
     {
 	    g_print("%s:%d\n", __FUNCTION__,__LINE__);
-        g_print("[%s]\n", g_variant_get_string(g_variant_lookup_value(properties, "Address", NULL), NULL));
-        g_print("  Name: %s\n", g_variant_lookup_value(properties, "Name", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(properties, "Name", NULL), NULL) : NULL);
-        g_print("  Alias: %s\n", g_variant_lookup_value(properties, "Alias", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(properties, "Alias", NULL), NULL) : NULL);
-        g_print("  Address: %s\n", g_variant_lookup_value(properties, "Address", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(properties, "Address", NULL), NULL) : NULL);
-        g_print("  Icon: %s\n", g_variant_lookup_value(properties, "Icon", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(properties, "Icon", NULL), NULL) : NULL);
-        g_print("  Class: 0x%x\n", g_variant_lookup_value(properties, "Class", NULL) != NULL ? g_variant_get_uint32(g_variant_lookup_value(properties, "Class", NULL)) : 0x0);
-        g_print("  LegacyPairing: %d\n", g_variant_lookup_value(properties, "LegacyPairing", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(properties, "LegacyPairing", NULL)) : FALSE);
-        g_print("  Paired: %d\n", g_variant_lookup_value(properties, "Paired", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(properties, "Paired", NULL)) : FALSE);
-        g_print("  RSSI: %d\n", g_variant_lookup_value(properties, "RSSI", NULL) != NULL ? g_variant_get_int16(g_variant_lookup_value(properties, "RSSI", NULL)) : 0x0);
+	    
     }
     
     g_variant_unref(changed_properties);
