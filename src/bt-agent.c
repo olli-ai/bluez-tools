6b24be400c5e473b305858cc15a3dfb74eaa905b
danghuutoan <danghuutoan2002@gmail.com>
2016-12-01 00:56:11 +0700

    update agent

--- src/bt-agent.c
+++ src/bt-agent.c
@@ -293,7 +293,7 @@ int main(int argc, char *argv[])
 	adapter_set_discoverable(adapter, g_variant_get_boolean(g_variant_new_boolean(TRUE)), &error);
 	exit_if_error(error);
 
-	guint object_sig_sub_id = g_dbus_connection_signal_subscribe(system_conn, "org.bluez", "org.freedesktop.DBus.ObjectManager", "InterfacesAdded", NULL, NULL, G_DBUS_SIGNAL_FLAGS_NONE, _manager_device_found, NULL, NULL);
+	guint object_sig_sub_id = g_dbus_connection_signal_subscribe(system_conn, "org.bluez", "org.freedesktop.DBus.ObjectManager", "InterfacesAdded", NULL, NULL, G_DBUS_SIGNAL_FLAGS_NONE, _manager_device_found, (gpointer) adapter_get_dbus_object_path(adapter), NULL);
     exit_if_error(error);
     g_print("%s\n", adapter_get_dbus_object_path(adapter));
 	guint prop_sig_sub_id = g_dbus_connection_signal_subscribe(system_conn, "org.bluez", "org.freedesktop.DBus.Properties", "PropertiesChanged", NULL, NULL, G_DBUS_SIGNAL_FLAGS_NONE, _adapter_property_changed, adapter, NULL);
