03a07ecefd77b88ff3c324aacdc97eee779774f5
danghuutoan <danghuutoan2002@gmail.com>
2016-11-30 15:33:52 +0700

    update agent

--- src/bt-agent.c
+++ src/bt-agent.c
@@ -79,6 +79,28 @@ static void _manager_device_found(GDBusConnection *connection, const gchar *send
     g_variant_unref(interfaces_and_properties);
 }
 
+
+static void _adapter_property_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data)
+{
+    // g_assert(user_data != NULL);
+    // GMainLoop *mainloop = user_data;
+    
+    // GVariant *changed_properties = g_variant_get_child_value(parameters, 1);
+    // GVariant *discovering_variant = g_variant_lookup_value(changed_properties, "Discovering", NULL);
+    // if(discovering_variant)
+    // {
+    //     const gboolean discovering = g_variant_get_boolean(discovering_variant);
+    //     if(!discovering)
+    //     {
+    //         g_main_loop_quit(mainloop);
+    //     }
+    //     g_variant_unref(discovering_variant);
+    // }
+    // g_variant_unref(changed_properties);
+
+    g_print("%s\r\n", __FUNCTION__);
+}
+
 // Not touching this for now. It seems to work.
 static void _read_pin_file(const gchar *filename, GHashTable *pin_hash_table, gboolean first_run)
 {
@@ -265,6 +287,9 @@ int main(int argc, char *argv[])
 	exit_if_error(error);
 
 	guint object_sig_sub_id = g_dbus_connection_signal_subscribe(system_conn, "org.bluez", "org.freedesktop.DBus.ObjectManager", "InterfacesAdded", NULL, NULL, G_DBUS_SIGNAL_FLAGS_NONE, _manager_device_found, (gpointer) adapter_get_dbus_object_path(adapter), NULL);
+    exit_if_error(error);
+
+	guint prop_sig_sub_id = g_dbus_connection_signal_subscribe(system_conn, "org.bluez", "org.freedesktop.DBus.Properties", "PropertiesChanged", adapter_get_dbus_object_path(adapter), NULL, G_DBUS_SIGNAL_FLAGS_NONE, _adapter_property_changed, NULL, NULL);
     exit_if_error(error);
 
 	AgentManager *agent_manager = agent_manager_new();
