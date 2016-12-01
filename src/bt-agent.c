1cd6fe6a5904116ebd466e31e231407bd0f42fd4
danghuutoan <danghuutoan2002@gmail.com>
2016-12-01 10:35:33 +0700

    update agent

--- src/bt-agent.c
+++ src/bt-agent.c
@@ -87,6 +87,7 @@ static void _manager_device_found(GDBusConnection *connection, const gchar *send
 
 static void _adapter_property_changed(GDBusConnection *connection, const gchar *sender_name, const gchar *object_path, const gchar *interface_name, const gchar *signal_name, GVariant *parameters, gpointer user_data)
 {
+	static gchar *current_device = NULL;
     g_assert(user_data != NULL);
     Adapter *adapter = user_data;
     
@@ -104,23 +105,33 @@ static void _adapter_property_changed(GDBusConnection *connection, const gchar *
 
 		if(g_variant_get_boolean(g_variant_lookup_value(device_properties, "Connected", NULL)))
 		{
-			g_print("device Connected\n");
+			// g_print("device Connected\n");
+			if(current_device == NULL)
+			{
+				current_device = g_variant_get_string(g_variant_lookup_value(device_properties, "Icon", NULL), NULL);
+				g_print(" Connected to %s", current_device);
+			}
+			else
+			{
+
+			}
+
 		}
 		else
 		{
 			g_print("device Disconnected\n");
 		}
-		
-		g_print("  Connected: %d\n", g_variant_lookup_value(device_properties, "Connected", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(device_properties, "Connected", NULL)) : FALSE);
-		g_print("  Name: %s\n", g_variant_lookup_value(device_properties, "Name", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Name", NULL), NULL) : NULL);
+
+		//g_print("  Connected: %d\n", g_variant_lookup_value(device_properties, "Connected", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(device_properties, "Connected", NULL)) : FALSE);
+		//g_print("  Name: %s\n", g_variant_lookup_value(device_properties, "Name", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Name", NULL), NULL) : NULL);
 		// g_print("  Alias: %s\n", g_variant_lookup_value(device_properties, "Alias", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Alias", NULL), NULL) : NULL);
-		g_print("  Address: %s\n", g_variant_lookup_value(device_properties, "Address", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Address", NULL), NULL) : NULL);
+		//g_print("  Address: %s\n", g_variant_lookup_value(device_properties, "Address", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Address", NULL), NULL) : NULL);
 		// g_print("  Icon: %s\n", g_variant_lookup_value(device_properties, "Icon", NULL) != NULL ? g_variant_get_string(g_variant_lookup_value(device_properties, "Icon", NULL), NULL) : NULL);
 		// g_print("  Class: 0x%x\n", g_variant_lookup_value(device_properties, "Class", NULL) != NULL ? g_variant_get_uint32(g_variant_lookup_value(device_properties, "Class", NULL)) : 0x0);
 		// g_print("  LegacyPairing: %d\n", g_variant_lookup_value(device_properties, "LegacyPairing", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(device_properties, "LegacyPairing", NULL)) : FALSE);
 		// g_print("  Paired: %d\n", g_variant_lookup_value(device_properties, "Paired", NULL) != NULL ? g_variant_get_boolean(g_variant_lookup_value(device_properties, "Paired", NULL)) : FALSE);
-		g_print("  RSSI: %d\n", g_variant_lookup_value(device_properties, "RSSI", NULL) != NULL ? g_variant_get_int16(g_variant_lookup_value(device_properties, "RSSI", NULL)) : 0x0);
-		g_print("\n");
+		//g_print("  RSSI: %d\n", g_variant_lookup_value(device_properties, "RSSI", NULL) != NULL ? g_variant_get_int16(g_variant_lookup_value(device_properties, "RSSI", NULL)) : 0x0);
+		//g_print("\n");
 		g_variant_unref(device_properties);
 
 		g_variant_unref(changed_properties);
